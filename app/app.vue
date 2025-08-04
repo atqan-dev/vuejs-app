26d88a36e07cefc0f9f727e61a67295006428844
Mahmoud Alattar-Atqan <110762683+atqan-dev@users.noreply.github.com>
2025-08-04 11:04:39 +0300

    change app.vue p tag
    تغير محتوي الصفحة الرئسيسية

--- app/app.vue
+++ app/app.vue
@@ -2,6 +2,14 @@
   <div>
     <NuxtRouteAnnouncer />
     <h1>that is test messages</h1>
-    <p>Add p tag to page </p>
+    <p>🔧 Features
+
+    Sync latest updates from GitHub
+    Store commit logs and app metadata
+    Verify app availability via Nginx (port/domain)
+    Handle dev/test/prod deployments
+    Track deployment success
+    Frontend dashboard integration
+</p>
   </div>
 </template>
